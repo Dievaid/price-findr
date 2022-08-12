c9e3d5ff84ade1eb822faa112a0ada7e575c9bbf
Dievaid <davidcapragiu.esports@gmail.com>
2022-08-12 18:22:36 +0300

    Animated form and background styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,39 @@
 //* Library includes
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 //* Stylesheet includes
-import "./components/stylesheets/NoBorder.css"
+import "./components/stylesheets/NoBorder.css";
+import "./components/stylesheets/Wave.css";
 
 //* Component includes
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import { Collapse } from "@mui/material";
 
-const App : React.FC = () => {
+//? Main component
+const App: React.FC = () => {
   const [isLogged, setLogged] = useState<boolean>(false);
-  
+  const [loginClicked, setLoginClicked] = useState<boolean>(false);
+  const [registerClicked, setRegisterClicked] = useState<boolean>(false);
+
   return (
     <div>
-      <Navbar isLoggedIn={isLogged}/>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <Navbar
+        isLoggedIn={isLogged}
+        setLoginClicked={setLoginClicked}
+        setRegisterClicked={setRegisterClicked}
+      />
+      <Collapse in={loginClicked} collapsedSize={150}>
+        <Form closeCallback={setLoginClicked} type="Login"></Form>
+      </Collapse>
+      <Collapse in={registerClicked} collapsedSize={150}>
+        <Form closeCallback={setRegisterClicked} type="Register"></Form>
+      </Collapse>
     </div>
   );
-}
-
+};
 export default App;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,23 +1,59 @@
-import * as React from 'react';
-
-import "./stylesheets/NoBorder.css";
-
-import { Container } from '@mui/system';
-import { AppBar } from '@mui/material';
+//* Library imports
+import React from "react";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import {
+  AppBar,
+  Button,
+  Grid,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 interface NavProps {
-    isLoggedIn: boolean
+  isLoggedIn: boolean;
+  setLoginClicked: React.Dispatch<React.SetStateAction<boolean>>
+  setRegisterClicked: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+const navbarColor = {
+  backgroundColor: "#ffffff00",
+  padding: 10
+};
+
+//? Navbar Component
 const Navbar : React.FC<NavProps> = (props) => {
-    return (
-      <Container>
-        <AppBar color="transparent"
-                position="sticky"
-                >
-                price findr.
-        </AppBar>
-      </Container>
-    );
+  const loginOnClick = () => {
+    props.setLoginClicked(true);
+    props.setRegisterClicked(false);
+  }
+
+  const registerOnClick = () => {
+    props.setRegisterClicked(true);
+    props.setLoginClicked(false);
+  }
+  return (
+    <AppBar color="default" style={navbarColor}>
+      <Toolbar>
+        <IconButton>
+          <MonetizationOnIcon />
+        </IconButton>
+        <Typography variant="h6" component="div">
+          price findr.
+        </Typography>
+        <Grid
+          container
+          columnSpacing={{ xs: 2, sm: 2, md: 3 }}
+          justifyContent="flex-end"
+        >
+          <Grid item>
+            {!props.isLoggedIn && <Button onClick={loginOnClick}>Login</Button>}
+            {!props.isLoggedIn && <Button onClick={registerOnClick}>Register</Button>}
+            {props.isLoggedIn && <Button>Logout</Button>}
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 };
 export default Navbar;
